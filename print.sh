1db69f27751025e37a89274bb84f1fc1db0b27b9
Noel Kuntze <noel@familie-kuntze.de>
2017-03-03 18:44:36 +0100

    Fix some stuff.

--- print.sh
+++ print.sh
@@ -4,18 +4,24 @@
 
 PRINTER="CLP-415NW"
 pages_orig=`pdfinfo "$1" | grep Pages | tr -s " " | cut -d " " -f 2`
+if [ -z ${pages_orig} ]
+then
+	exit
+fi
 
-if [ $pages_orig -gt 1 ]
-	let pages_orig_dec=$pages_orig-1
+if [ ${pages_orig} -gt 1 ]
+then
+	let pages_orig_dec=${pages_orig}-1
 else
-	pages_orig_dec=$pages_orig
+	pages_orig_dec=${pages_orig}
 fi
 
 if [ $pages_orig_dec -gt 1 ]
-	pages=`printf "1-%d" $pages_orig_dec`
+then
+	pages=`printf "1-%d" ${pages_orig_dec}`
 else
 	pages=1
 fi
 
 lpr -d "${PRINTER}" -o page-range=${pages}
-echo "$1"
+echo "$1 pages: ${pages}"
